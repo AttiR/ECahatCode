2603f50c5b92a37bc9930d9fe4b1029f99abbfe4
AttiR <77799634+AttiR@users.noreply.github.com>
2021-10-21 00:02:56 +0300

    search result pulling

--- search.php
+++ search.php
@@ -1,14 +1,34 @@
 <?php
 include './config/dbcon.php';
 ?>
-<header>
-    <?php include './view/header.php'?>
+
+
+</d<header>
+<?php include './view/header.php'?>
 </header>
+
 <section>
-    <div class="container" style="min-height: 60vh;">
-        <h1 class="my-3">Search Result for <?php echo $_GET['search'] ?></h1>
-        <?php
+
+    <div class="container" style="margin-top:60px">
+        <h1 class="my-3">Search Result for <em style="color: orange;">
+                <?php echo $_GET['search'] ?> </em>
+        </h1>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">
+                        s.no
+                    </th>
+                    <th scope="col">Thread Title</th>
+                    <th scope="col">Thread Description</th>
+                </tr>
+            </thead>
+
+
+            <?php
 $search = $_GET['search'];
+$search = mysqli_real_escape_string($connect, $search);
 /*$sql = "SELECT * FROM code_threads where code_thread_title like '$search%' OR
 code_thread_desc LIKE '$search%'";*/
 // We will use Full_text Search Technique
@@ -20,25 +40,21 @@ while ($row = mysqli_fetch_assoc($result)) {
     $desc = $row['code_thread_desc'];
     $thread_id = $row['code_thread_id'];
 
-    echo ' <div>
-            <h3>
-            <a href="thread-detail.php?threadid= ' . $thread_id . '">
-            ' . $title . '
-            </a>
-            </h3>
-            <p>
-            ' . $desc . '
-            </p>
-            </div>';
+    echo '
 
-}
-//$row = $connect->query($sql) or die('insert failed<br>' . $sql . '<br>' . mysqli_error($connect));
-?>
-
+            <tr>
+                <td style="width: 20%;">' . $thread_id . '
+                </td>
+                <td style="width:30%"><a class = "text-dark"  style= "text-decoration:none;" href="thread-detail.php?threadid= ' . $thread_id . '">
+                ' . $title . '
+                </a></td>
+                <td style="width: 50%;" >' . $desc . '</td>
+            </tr>
+        ';
+// $row = $connect->query($sql) or die('insert failed<br>' . $sql . '<br>' . mysqli_error($connect));
+}?>
+        </table>
 
     </div>
-
 </section>
-
-
 <?php include './view/footer.php'?>
--- view/header.php
+++ view/header.php
@@ -5,24 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Css -->
-    <link rel="stylesheet" href="./css/styles.css?<?php echo time(); ?>">
-
-    <!-- Bootsrap Css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- Bootsrap JavaScript Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-
-    <!-- Fontawsome -->
-    <script src="https://kit.fontawesome.com/d54712eab9.js" crossorigin="anonymous"></script>
-
-    <title>ChatCodeE- Register</title>
+    <title>Document</title>
 </head>
+<link rel="stylesheet" href="./css/styles.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
+<!-- FontAwsome -->
+<script src="https://kit.fontawesome.com/d54712eab9.js" crossorigin="anonymous"></script>
+
 
 <body>
 
@@ -116,4 +109,4 @@ echo '
 
         </div>
     </div>
-</nav>'; ?>
+</nav>';
