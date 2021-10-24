61ddc8dd26e9ca05959bc4ab0e5e51119a9eac5a
AttiR <77799634+AttiR@users.noreply.github.com>
2021-10-24 12:09:53 +0300

    modifies styling

--- profile.php
+++ profile.php
@@ -24,12 +24,13 @@ include './config/dbcon.php';
 
             <div class="card card-prof" style="border:none">
                 <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
-                    <div class="card card2">
+                    <div class="card card2"
+                        style="background: rgba(231, 231, 241, 0.4);padding:2rem; border-bottom: 3px solid purple">
                         <?php
 
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
     echo '<div class=" image d-flex flex-column justify-content-center align-items-center">
-                        <button class="btn probtn btn-secondary my-4"> <img class="imgpro"
+                        <button class="btn probtn btn-secondary my-4" > <img class="imgpro"
                                 src="https://www.pngitem.com/pimgs/m/35-350426_profile-icon-png-default-profile-picture-png-transparent.png"
                                 height="100" width="100" />
                         </button>
@@ -38,13 +39,13 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
                                 class="idd1">' . $_SESSION['uid'] . '</span> <span><i class="fa fa-copy"></i></span>
                         </div>
 
-                        <div class=" d-flex mt-2"> <a class="btn btn-dark" href="./userManagement/recover.php">
+                        <div class=" d-flex mt-2"> <a class="btn btn-dark" style="margin: 2rem auto" href="./userManagement/recover.php">
                                 Change Password
                             </a>
                         </div>
-                        <div class="text mt-3"> <p style = "margin: auto 2opx">Manage profile, basic information we have saved.
+                        <div class="text mt-3"> <p style = "margin: auto 2opx; letter-spacing:0.1em">Manage profile, basic information we have saved.
 
-                                artwork.<br> Please remember the forum ethics. </p>
+                                <br> Please remember the forum ethics. </p>
                         </div>
 
                     </div>';
